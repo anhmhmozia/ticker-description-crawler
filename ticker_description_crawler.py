a436d495ff82bdbcafa73ce006346deb0cea3017
AnhMH <anh.mh@moziasoft.com>
2025-04-03 15:44:52 +0700

    Init

---
+++ ticker_description_crawler.py
@@ -0,0 +1,30 @@
+from yahooquery import Ticker
+from datetime import datetime
+import mysql_connection
+from configs import cfg
+cfg.run()
+
+def ticker_crawler():
+    items = []
+    today = datetime.today().strftime("%Y-%m-%d")
+    symbols = mysql_connection.select_normal('tickers', ['ticker'], 'WHERE description_crawl_at is null', 'scalemarketcap desc', 1000)
+    for s in symbols:
+        symbol = s[0]
+        print(symbol)
+        try:
+            ticker = Ticker(symbol)
+            tickerInfo = ticker.asset_profile[symbol]
+            description = tickerInfo['longBusinessSummary'] or ''
+            website = tickerInfo['website'] or ''
+            items.append((symbol, description, website, today))
+        except Exception as e:
+            print('Error')
+            continue
+
+    mysql_connection.insert_or_update('tickers', [
+                                          'ticker', 'description', 'website', 'description_crawl_at'],
+                                          ['ticker'], items)
+
+if __name__ == '__main__':
+    ticker_crawler()
+    mysql_connection.disconnection()
